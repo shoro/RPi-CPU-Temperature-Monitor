07226002a020084b66476751d78f1a75f2831b8a
SRDC <sorodocul@gmail.com>
2024-08-28 12:46:44 +0300

    Update cpumon.py

--- cpumon.py
+++ cpumon.py
@@ -1,10 +1,11 @@
-#  Created by SRDC
-
 import os
 import time
 
 while True:
     temperature = float(os.popen('vcgencmd measure_temp').readline().replace("temp=","").replace("'C",""))
     voltage = float(os.popen('vcgencmd measure_volts core').readline().replace("volt=","").replace("V",""))
+#   print("Temp: ", temperature, "°C", " @ ", voltage, "V")
+#   print("Temp.: ", temperature, "°C", "|", "Volt.: ", voltage, "V")
     print(temperature, "°C", "|", voltage, "V")
+#   print("Temperature: ", temperature, "°C", "\tVoltage: ", voltage, "V")
     time.sleep(1)
